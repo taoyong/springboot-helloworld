cb4f87bb19713382a06f26d10eae22e08bb5239e
taoyong <taoyong0728@126.com>
2018-10-17 03:06:10 +0800

    fix：文件上传和比对功能

--- src/main/java/com/keeper/springBootHelloWorld/controller/AppController.java
+++ src/main/java/com/keeper/springBootHelloWorld/controller/AppController.java
@@ -5,10 +5,17 @@ import com.keeper.springBootHelloWorld.dto.ResponseWrapper;
 import com.keeper.springBootHelloWorld.dto.UserLoginReq;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.io.IOUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Slf4j
 @RestController
@@ -25,5 +32,42 @@ public class AppController {
         log.info("用户登录,userLoginReq={}",userLoginReq);
         return ResponseWrapper.succ();
     }
+
+    /**
+     * local-requrl:http://localhost:8080/helloworld/app/v1/greeting
+     * @return
+     */
+    @RequestMapping(value = "/greeting")
+    public ModelAndView test(ModelAndView mv) {
+        mv.setViewName("/index");
+        mv.addObject("title","欢迎来到我的个人空间!");
+        return mv;
+    }
+
+
+    @PostMapping("/upload")
+    public ModelAndView singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("uploadStatus");
+
+
+        try {
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            String content = IOUtils.toString(bytes,"utf-8");
+            log.info("content={}",content);
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        modelAndView.addObject("fileName",file.getOriginalFilename());
+
+        return modelAndView;
+    }
+
 }
 
